ffb332d45b0b4c7c458e2c49bb224ac3ceded1e2
Vishal Patil <vishpat@gmail.com>
2024-02-26 22:05:16 -0500

    Checkpoint

--- logistic-regression/src/main.rs
+++ logistic-regression/src/main.rs
@@ -5,17 +5,8 @@ use clap::Parser;
 use core::panic;
 use std::char::from_digit;
 use std::fs::File;
-use std::{array, iter};
 use std::rc::Rc;
-use polars::prelude::*;
-
-struct Dataset {
-    pub training_data: Tensor,
-    pub training_labels: Tensor,
-    pub test_data: Tensor,
-    pub test_labels: Tensor,
-    pub feature_cnt: usize,
-}
+use std::{array, iter};
 
 // Implement Logistic Regression model using Gradient Descent
 // https://www.youtube.com/watch?v=4u81xU7BIOc
@@ -75,64 +66,36 @@ impl LogisticRegression {
 const LEARNING_RATE: f32 = 0.01;
 const ITERATIONS: i32 = 100000;
 
-fn income_dataset(
-    training_file_path: &str,
-    test_file_path: &str,
-    device: &Device,
-) -> Result<Dataset> {
-    // https://www.kaggle.com/datasets/nimapourmoradi/adult-incometrain-test-dataset/data
-
-    let dataset = CsvReader::from_path(training_file_path)?
-        .has_header(true)
-        .finish()?;
-
-    println!("{:?}", dataset);
-
-
-    let dummy1 = Tensor::from_slice(&[1.0, 2.0], (1,), device)?;
-    let dummy2 = Tensor::from_slice(&[1.0, 2.0], (1,), device)?;
-    let dummy3 = Tensor::from_slice(&[1.0, 2.0], (1,), device)?;
-    let dummy4 = Tensor::from_slice(&[1.0, 2.0], (1,), device)?;
-
-    Ok(Dataset {
-        training_data: dummy1,
-        training_labels: dummy2,
-        test_data: dummy3,
-        test_labels: dummy4,
-        feature_cnt: 0,
-    })
-}
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(long)]
-    training_csv: String,
-
-    #[arg(long)]
-    test_csv: String,
-}
 fn main() -> Result<()> {
-    let args = Args::parse();
-
-    let training_file_path = args.training_csv;
-    let test_file_path = args.test_csv;
-
     let device = Rc::new(Device::cuda_if_available(0)?);
 
-    let dataset = income_dataset(&training_file_path, &test_file_path, &device)?;
+    let dataset = candle_datasets::vision::mnist::load()?;
+    let train_images = dataset.train_images;
+    let train_labels = dataset.train_labels;
 
-//    let mut model = LogisticRegression::new(dataset.feature_cnt, device)?;
-//
-//    for _ in 0..ITERATIONS {
-//        model.train(
-//            &dataset.training_data,
-//            &dataset.training_labels,
-//            LEARNING_RATE,
-//        )?;
-//    }
-//
-//    let predictions = model.hypothesis(&dataset.test_data)?;
-//
+    println!("Training data shape: {}", train_images);
+    println!("Training labels shape: {}", train_labels);
+
+    let train_labels_vec = train_labels
+        .to_vec1::<u8>()?
+        .into_iter()
+        .map(|x| if x == 0 { 1.0 } else { 0.0 })
+        .collect::<Vec<f32>>();
+    println!("{:?}", train_labels_vec);
+    let len = train_labels_vec.len();
+    let train_labels = Tensor::from_vec(train_labels_vec, (len,), &device)?;
+
+    //    let mut model = LogisticRegression::new(dataset.feature_cnt, device)?;
+    //
+    //    for _ in 0..ITERATIONS {
+    //        model.train(
+    //            &dataset.training_data,
+    //            &dataset.training_labels,
+    //            LEARNING_RATE,
+    //        )?;
+    //    }
+    //
+    //    let predictions = model.hypothesis(&dataset.test_data)?;
+    //
     Ok(())
 }
