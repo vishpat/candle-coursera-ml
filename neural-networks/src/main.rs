51fb2ace52ef17d0ef51fd9e88cbbbbb358fba5b
Vishal Patil <vishpat@gmail.com>
2024-03-02 18:10:52 -0500

    Checkpoint

--- neural-networks/src/main.rs
+++ neural-networks/src/main.rs
@@ -1,8 +1,7 @@
 use anyhow::Result;
-use candle::{Device, Tensor, D};
-use candle_nn::{loss, ops, Conv2d, Linear, Module, ModuleT, Optimizer, VarBuilder, VarMap};
-use clap::{Parser, ValueEnum};
-use rand::prelude::*;
+use candle_core::{DType, Device, Tensor, D};
+use candle_nn::{loss, ops, Linear, Module, Optimizer, VarBuilder, VarMap};
+use clap::Parser;
 use std::rc::Rc;
 
 const IMAGE_DIM: usize = 28 * 28;
@@ -13,7 +12,6 @@ struct Dataset {
     pub training_labels: Tensor,
     pub test_data: Tensor,
     pub test_labels: Tensor,
-    pub feature_cnt: usize,
 }
 
 fn load_tensors(csv: &str, device: &Device) -> Result<(Tensor, Tensor)> {
@@ -48,7 +46,6 @@ fn load_dataset(train_csv: &str, test_csv: &str, device: &Device) -> Result<Data
         training_labels,
         test_data,
         test_labels,
-        feature_cnt: IMAGE_DIM,
     })
 }
 #[derive(Parser, Debug)]
@@ -66,7 +63,7 @@ struct Args {
 
     // The learning rate
     #[arg(long, default_value = "0.01")]
-    learning_rate: f32,
+    learning_rate: f64,
 
     // The regularization parameter
     #[arg(long, default_value = "0.01")]
@@ -77,10 +74,60 @@ struct Args {
     epochs: i32,
 }
 
+struct Mlp {
+    ln1: Linear,
+    ln2: Linear,
+}
+
+impl Mlp {
+    fn new(vs: VarBuilder) -> Result<Self> {
+        let ln1 = candle_nn::linear(IMAGE_DIM, 100, vs.pp("ln1"))?;
+        let ln2 = candle_nn::linear(100, LABELS, vs.pp("ln2"))?;
+        Ok(Self { ln1, ln2 })
+    }
+
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        let xs = self.ln1.forward(xs)?;
+        let xs = xs.relu()?;
+        Ok(self.ln2.forward(&xs)?)
+    }
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
     let device = Rc::new(Device::cuda_if_available(0)?);
     let dataset = load_dataset(&args.train_csv, &args.test_csv, &device)?;
+
+    let varmap = VarMap::new();
+    let vs = VarBuilder::from_varmap(&varmap, DType::F32, &device);
+    let model = Mlp::new(vs)?;
+    let mut sgd = candle_nn::SGD::new(varmap.all_vars(), args.learning_rate)?;
+
+    let test_images = dataset.test_data.to_device(&device)?;
+    let test_labels = dataset
+        .test_labels
+        .to_dtype(DType::U32)?
+        .to_device(&device)?;
+    for epoch in 1..args.epochs {
+        let logits = model.forward(&dataset.training_data)?;
+        let log_sm = ops::log_softmax(&logits, D::Minus1)?;
+        let loss = loss::nll(&log_sm, &dataset.training_labels)?;
+        sgd.backward_step(&loss)?;
+
+        let test_logits = model.forward(&test_images)?;
+        let sum_ok = test_logits
+            .argmax(D::Minus1)?
+            .eq(&test_labels)?
+            .to_dtype(DType::F32)?
+            .sum_all()?
+            .to_scalar::<f32>()?;
+        let test_accuracy = sum_ok / test_labels.dims1()? as f32;
+        println!(
+            "{epoch:4} train loss: {:8.5} test acc: {:5.2}%",
+            loss.to_scalar::<f32>()?,
+            100. * test_accuracy
+        );
+    }
 
     Ok(())
 }
