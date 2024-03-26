f66133cc8964a89fff813303e51815872ea7e5a2
Vishal Patil <vishpat@gmail.com>
2024-03-26 19:53:40 -0400

    Revert "Initial Check-in for recommender system"
    This reverts commit c64bc744f485520897d79ce5d9a7331e5717086e.

--- recommender-system/target/debug/deps/recommender_system-fe75debadc8842b8.d
+++
@@ -1,5 +0,0 @@
-/home/vishpat/projects/candle-coursera-ml/recommender-system/target/debug/deps/librecommender_system-fe75debadc8842b8.rmeta: src/main.rs
-
-/home/vishpat/projects/candle-coursera-ml/recommender-system/target/debug/deps/recommender_system-fe75debadc8842b8.d: src/main.rs
-
-src/main.rs:
