ae4a10e448fad7eac411c481d9f36ca89b572ce8
Vishal Patil <vishpat@gmail.com>
2024-03-31 16:42:43 -0400

    Able to get all data in tensors

--- recommender-system/src/main.rs
+++ recommender-system/src/main.rs
@@ -43,6 +43,60 @@ fn load_ratings(file_path: &str) -> Result<(HashSet<u32>, HashSet<u32>, HashSet<
     Ok((users, movies, ratings))
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Data CSV file from https://www.kaggle.com/c/eecs498/data
+    #[arg(long)]
+    ratings_csv: String,
+
+    // Number of epochs to train
+    #[arg(long, default_value = "10")]
+    epochs: u32,
+
+    // Learning rate
+    #[arg(long, default_value = "0.01")]
+    lr: f32,
+
+    // Regularization factor
+    #[arg(long, default_value = "0.01")]
+    reg: f32,
+
+    // Number of features
+    #[arg(long, default_value = "100")]
+    n_features: usize,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let device = Device::cuda_if_available(0)?;
+
+    let (users, movies, ratings) = load_ratings(&args.ratings_csv).unwrap();
+    let users: Vec<u32> = users.into_iter().collect();
+    let movies: Vec<u32> = movies.into_iter().collect();
+    let ratings: Vec<Rating> = ratings.into_iter().collect();
+
+    let n_users = users.len();
+    let n_movies = movies.len();
+
+    let mut Y = vec![vec![0.0; n_movies as usize]; n_users as usize];
+    let mut R = vec![vec![0.0; n_movies as usize]; n_users as usize];
+
+    for rating in ratings.iter() {
+        let i = users.iter().position(|&x| x == rating.movie).unwrap();
+        let j = movies.iter().position(|&x| x == rating.user).unwrap();
+        Y[i][j] = rating.rating();
+        R[i][j] = 1.0;
+    }
+
+    let Y = Y.iter().flatten().copied().collect::<Vec<f32>>();
+    let Y = Tensor::from_slice(&Y, (n_movies, n_users), &device)?;
+
+    let R = R.iter().flatten().copied().collect::<Vec<f32>>();
+    let R = Tensor::from_slice(&R, (n_movies, n_users), &device)?;
+
+    let X = Tensor::randn(0f32, 1., (n_movies, args.n_features), &device)?;
+    let Theta = Tensor::randn(0f32, 1., (n_users, args.n_features), &device)?;
+
+    Ok(())
 }
